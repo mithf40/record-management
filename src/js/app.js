4bf1fcf7afdedc5c33991e85494ec92b8a4479f9
Mithlesh Kumar <kmithleshf40@gmail.com>
2019-04-09 12:01:39 +0530

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,46 @@
+App = {
+    web3Provider: null,
+    contracts: {},
+  
+    init: async function() {
+      
+  
+      return await App.initWeb3();
+    },
+  
+    initWeb3: async function() {
+      /*
+       * Replace me...
+       */
+  
+      return App.initContract();
+    },
+  
+    initContract: function() {
+      /*
+       * Replace me...
+       */
+  
+      return App.bindEvents();
+    },
+  
+    bindEvents: function() {
+      $(document).on('click', '.btn-provider', App.handleProvider);
+    //   $(document).on('click', '.btn-patient', App.handlePatient);
+    },
+
+    handleProvider: function(){
+        event.preventDefault();
+
+        window.location.href = "https://google.com";
+    }
+  
+  
+  };
+  
+  $(function() {
+    $(window).load(function() {
+      App.init();
+    });
+  });
+  
